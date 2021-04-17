d9ada5301ab72095344f46ceea37790413d21e44
kataer <2528300657@qq.com>
2021-04-17 22:28:37 +0800

    leetcode_001 commit

---
+++ src/main/java/com/kataer/leetcode/Leetcode_001.java
@@ -0,0 +1,11 @@
+package com.kataer.leetcode;
+
+/**
+ * @ClassName Leetcode_001
+ * @Description: TODO
+ * @Author kataer
+ * @Date 2021/4/17 22:28
+ * @Version V1.0
+ **/
+public class Leetcode_001 {
+}
